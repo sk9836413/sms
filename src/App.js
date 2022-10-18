9c257d08a2862aa0befb8c2dd9edcdfd58abdacd
sk9836413 <83128063+sk9836413@users.noreply.github.com>
2022-10-18 17:44:15 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import AddmissionForm from './Forms/AddmissionForm.js'
+import AdmissionForm from './Forms/AdmissionForm';
+import Admission from './Forms/Admission.js';
 
 function App() {
   return (
     <>
-    <AddmissionForm/>
+    <Admission/>
     
     </>
     
